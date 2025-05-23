4ab270c6df6e1a9bfb30c11908ca9af0030118e1
Elisse <elissec02@gmail.com>
2025-05-24 01:45:42 +0800

    Tester2 Slash Architect Edits

--- src/EndToEndSample/SampleAddedClassGit.java
+++ src/EndToEndSample/SampleAddedClassGit.java
@@ -4,6 +4,11 @@ public class SampleAddedClassGit extends Scope {
 	
 	public static void main(String[] args) {
 	System.out.println("Tester 2 Edited this and added a class~!!");
-	 
+	
+	
+	System.out.println("Tester 2 Edited this in another branch! Noice ");
+	System.out.println("Tester 2 Edited this in another branch! Noice ");
+	System.out.println("Tester 2 Edited this in another branch! Noice ");
+	
 	}
 }
---
+++ src/EndToEndSample/SampleAddedClassGit_Another.java
@@ -0,0 +1,10 @@
+package EndToEndSample;
+
+public class SampleAddedClassGit_Another extends Scope {
+	
+	public static void main(String[] args) {
+	System.out.println("Tester 2 Edited this and added a class~!!");
+	System.out.println("Tester 2 Makes this in another Branch");
+	 
+	}
+}
