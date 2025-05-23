0e7b38171613d2738d4aa42da17549a21c13b780
Elisse <elissec02@gmail.com>
2025-05-24 00:46:44 +0800

    Tester Edit 3

--- src/EndToEndSample/SampleAddedClassGit.java
+++ src/EndToEndSample/SampleAddedClassGit.java
@@ -2,6 +2,8 @@ package EndToEndSample;
 
 public class SampleAddedClassGit extends Scope {
 	
+	public static void main(String[] args) {
 	System.out.println("Tester 2 Edited this and added a class~!!");
 	 
+	}
 }
